d56fda7779268de8ce332279123902eaa979a9ac
hp? <90970059+motionarium@users.noreply.github.com>
2022-01-29 13:05:32 +0300

    tested

---
+++ typed.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+new TypeIt("#type", {
+    speed: 125,
+    loop: true,
+    nextStringDelay: 750,
+    deleteSpeed: 150,
+    })
+    .type('Я <span class="name">motionarium</span>!')
+    .delete(".name")
+    .type('hackerpepe!')
+    .go();
+})
