b2f97101371fa309ebb73a8c67e7a08f88244658
hp? <90970059+motionarium@users.noreply.github.com>
2022-01-29 14:11:05 +0300

    lmao

--- typed.js
+++ typed.js
@@ -11,3 +11,18 @@ new TypeIt("#type", {
     .type('hackerpepe!')
     .go();
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+    new TypeIt("#type2", {
+        speed: 125,
+        loop: true,
+        nextStringDelay: 750,
+        deleteSpeed: 150,
+        cursor: false,
+        })
+        .type('Страница не найдена!')
+        .delete()
+        .type('o_O')
+        .go();
+    })
+    
